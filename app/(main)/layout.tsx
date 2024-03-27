2af2e93f7b112f9d6419c197539e80b062be2453
selimdilsad <selimdilsadercan@gmail.com>
2024-03-27 16:57:38 +0300

    fix: changed header name to navbar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,10 @@
-import MobileSidebar from "@/components/nav/MobileSidebar";
+import MobileNavbar from "@/components/nav/MobileNavbar";
 import Sidebar from "@/components/nav/Sidebar";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <MobileSidebar />
+      <MobileNavbar />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
